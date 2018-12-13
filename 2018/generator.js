c2a525ae315050b2738230f7ee2b802a4eb4d12b
Cruyun <1045671796@qq.com>
2018-12-13 22:43:01 +0800

    add generator

---
+++ 2018/generator.js
@@ -0,0 +1,58 @@
+/**
+ * 
+ * @param {Function} cb 
+ */
+
+function generator(cb) {
+  return (
+    function() {
+      var object =  {
+        next: 0,
+        stop: function() {}
+      };
+      return {
+        next: function() {
+          var ret = cb(object);
+          if (ret === undefined) {
+            return {value: undefined, done: true}
+          }
+          return {
+            value: ret,
+            done: false
+          }
+        }
+      }
+    })();
+}
+
+/**
+ * Method2
+ */
+class RangeIterator {
+  constructor(start, stop) {
+    this.value = start;
+    this.stop = stop;
+  }
+
+  [Symbol.iterator]() { return this; }
+
+  next() {
+    var value = this.value;
+    if (value < this.stop) {
+      this.value++;
+      return {done: false, value: value};
+    } else {
+      return {done: true, value: undefined};
+    }
+  }
+}
+
+// Return a new iterator that counts up from 'start' to 'stop'.
+function range(start, stop) {
+  return new RangeIterator(start, stop);
+}
+
+for (var value of range(0, 3)) {
+  console.log(value)
+}
+
